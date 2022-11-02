b49d332d4674c9ed28c6934070dada40fd6a912f
ImZero00 <222190008@student.unaki.ac.id>
2022-11-02 10:09:05 +0700

    menambah form pendaftaran online

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-AOS.init();
+const galleryImage = document.querySelectorAll('.gallery-img');
+
+galleryImage.forEach((img, i) => {
+  img.dataset.aos = 'fade-up';
+  // delay
+  img.dataset.aosDelay = i * 100;
+});
+
+AOS.init({
+  duration: 1000,
+  once: true,
+});
+
+// const galleryImage = document.querySelectorAll('.gallery-img');
+
+// galleryImage.forEach ((img, i) => {
+//     img.dataset.aos = 'fade-up';
+
+//     img.dataset.asoDelay = i * 100;
+// });
